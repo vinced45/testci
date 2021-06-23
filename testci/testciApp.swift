0fa4d086b8169b8b2b9cc35b96364b0bd34af680
Vince Davis <vince.davis@me.com>
2021-06-23 17:57:36 -0500

    Initial Commit

---
+++ testci/testciApp.swift
@@ -0,0 +1,17 @@
+//
+//  testciApp.swift
+//  testci
+//
+//  Created by Vince Davis on 6/23/21.
+//
+
+import SwiftUI
+
+@main
+struct testciApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
